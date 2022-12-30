ea13e92d42291af6a85d08003d8d413ea87aa8e5
brensley <brensley.foster@gmail.com>
2022-12-30 11:44:30 -0500

    update to code

--- travispull.sh
+++ travispull.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 # This script will pull an image from an Amazon ECR repository and deploy it using Travis-ci
-
 #!/bin/bash
 
-# Login to AWS ECR registry
-$(aws ecr get-login --no-include-email --region us-east-2)
+# Install the AWS CLI and authenticate with AWS ECR
+pip install awscli
+$(aws ecr get-login --no-include-email --region us-east-1)
 
-# Pull the latest version of the Docker image from the AWS ECR registry
-docker pull Y647269089792.dkr.ecr.us-east-1.amazonaws.com/my-repository:latest
+# Pull the latest image from the AWS ECR repository
+docker pull m647269089792.dkr.ecr.us-east-1.amazonaws.com/my-image:latest
 
-# Run the Docker image
-docker run -d --name bftesting -p 8080:80 647269089792.dkr.ecr.us-east-1.amazonaws.com/bftesting:latest
+# Build a new Docker image based on the current codebase
+docker build -t imagetest .
+
+# Tag the new image with the same tag as the latest image in the AWS ECR repository
+docker tag imagetest 647269089792.dkr.ecr.us-east-1.amazonaws.com/my-image:latest
+
+# Push the new image to the AWS ECR repository
+docker push 647269089792.dkr.ecr.us-east-1.amazonaws.com/my-image:latest
