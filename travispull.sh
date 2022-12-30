4c5d3a81c99f3add7d8b01e4eb1e7d1303d14ff6
brensley <brensley.foster@gmail.com>
2022-12-30 10:44:59 -0500

    Pull and deploy

--- travispull.sh
+++ travispull.sh
@@ -2,15 +2,13 @@
 
 # This script will pull an image from an Amazon ECR repository and deploy it using Travis-ci
 
-# Set the AWS credentials
-export AWS_ACCESS_KEY_ID="your-access-key-id"
-export AWS_SECRET_ACCESS_KEY="your-secret-access-key"
+#!/bin/bash
 
-# Get the AWS ECR login command
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin https://aws_account_id.dkr.ecr.us-east-1.amazonaws.com
+# Login to AWS ECR registry
+$(aws ecr get-login --no-include-email --region us-east-2)
 
-# Pull the image
-docker pull aws_account_id.dkr.ecr.us-east-1.amazonaws.com/your-image-name:latest
+# Pull the latest version of the Docker image from the AWS ECR registry
+docker pull Y647269089792.dkr.ecr.us-east-1.amazonaws.com/my-repository:latest
 
-# Deploy the image using Travis-ci--
-travis deploy --com AWS_ECR --image aws_account_id.dkr.ecr.us-east-1.amazonaws.com/your-image-name:latest
+# Run the Docker image
+docker run -d --name bftesting -p 8080:80 647269089792.dkr.ecr.us-east-1.amazonaws.com/bftesting:latest
