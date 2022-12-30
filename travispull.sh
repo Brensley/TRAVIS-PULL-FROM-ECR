3472d920274e7c7b9b28277d6a371e0997cad782
brensley <brensley.foster@gmail.com>
2022-12-30 12:10:17 -0500

    commiting anohter change

--- travispull.sh
+++ travispull.sh
@@ -1,20 +1,25 @@
-#!/bin/bash
-
 # This script will pull an image from an Amazon ECR repository and deploy it using Travis-ci
+
 #!/bin/bash
 
-# Install the AWS CLI and authenticate with AWS ECR
-pip install awscli
-$(aws ecr get-login --no-include-email --region us-east-2)
+# Login to AWS ECR
+$(aws ecr get-login --region us-east-2 --no-include-email)
 
-# Pull the latest image from the AWS ECR repository
-docker pull m647269089792.dkr.ecr.us-east-1.amazonaws.com/my-image:latest
+# Set the repository URI and image tag
+REPO_URI=0647269089792.dkr.ecr.us-east-1.amazonaws.com/20221230155505-a9bb04859d25c483d39afee2e24a02bebed3ff6a=latest
 
-# Build a new Docker image based on the current codebase
-docker build -t imagetest .
+# Pull the image from ECR
+docker pull $REPO_URI:$IMAGE_TAG
 
-# Tag the new image with the same tag as the latest image in the AWS ECR repository
-docker tag imagetest 647269089792.dkr.ecr.us-east-1.amazonaws.com/my-image:latest
+# Set the task definition and cluster names
+TASK_DEFINITION=my-task-definition
+CLUSTER_NAME=my-ecs-cluster
 
-# Push the new image to the AWS ECR repository
-docker push 647269089792.dkr.ecr.us-east-1.amazonaws.com/my-image:latest
+# Register the task definition
+aws ecs register-task-definition --cli-input-json file://task-definition.json
+
+# Update the service with the new task definition
+aws ecs update-service --cluster $CLUSTER_NAME --service my-service --task-definition $TASK_DEFINITION
+This script first logs in to AWS ECR using the aws ecr get-login command. It then sets the repository URI and image tag for the image that we want to pull. Next, it uses the docker pull command to pull the image from ECR.
+
+After pulling the image, the script sets the task definition and cluster names that will be used to deploy the image to ECS. It then registers the task definition using the aws ecs register-task-definition command and the task-definition.json file. Finally, it updates the ECS service with the new task definition using the aws ecs update-service command.
